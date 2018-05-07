fcb9e6be1b5dc32f1632889f0090057114e1713f
Markus Oberlehner <markus.oberlehner@karriere.at>
2018-05-07 09:59:35 +0200

    Fix problems in combination with routing
    See #1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <page-home></page-home>
+    <app-modal></app-modal>
   </div>
 </template>
 
 <script>
+import AppModal from './components/AppModal';
 import PageHome from './components/PageHome';
 
 export default {
   name: 'app',
   components: {
+    AppModal,
     PageHome,
   },
 };
--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="c-pageHome">
     <button class="c-pageHome__login" @click="showModal('ModalLogin')">Login</button>
-
-    <app-modal></app-modal>
   </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex';
 
-import AppModal from './AppModal';
-
 export default {
   name: 'PageHome',
-  components: {
-    AppModal,
-  },
   methods: {
     ...mapMutations(['showModal']),
   },
